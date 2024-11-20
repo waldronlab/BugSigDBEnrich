34667719ef01efbef69d2459d539f7a5bd38b6ab
sdgamboa <samuel.gamboa.tuz@gmail.com>
2024-11-19 19:34:30 -0500

    Reduce number of files for bsdb code

--- R/server_bsdb.R
+++ R/server_bsdb.R
@@ -1,4 +1,5 @@
 
+# Results -----------------------------------------------------------------
 bsdbResult <- function(input, output, inputSigFun, bsdb, dat, sigs_rval) {
     inputSig <- inputSigFun()
     
@@ -102,5 +103,74 @@ bsdbResult <- function(input, output, inputSigFun, bsdb, dat, sigs_rval) {
                 sep = "\t"
             )
         }
+    )
+}
+
+# Options -----------------------------------------------------------------
+bsdbSigOptionsServer <- function(input, session) {
+    list(
+        shiny::observeEvent(input$bsdb_rank_mixed, {
+            if (input$bsdb_rank_mixed) {
+                shiny::updateCheckboxGroupInput(
+                    session, "bsdb_rank", selected = rankOptions()
+                )
+            } else {
+                shiny::updateCheckboxGroupInput(
+                    session, "bsdb_rank", selected = character(0)
+                )
+            }
+        }),
+        shiny::observe({
+            if (length(input$bsdb_rank) > 1)
+                shiny::updateRadioButtons(
+                    session, "bsdb_exact", selected = TRUE
+                )
+        })
+    )
+}
+
+# Help --------------------------------------------------------------------
+bsdbSigOptionsHelp <- function(input) {
+    list(
+        shiny::observeEvent(input$bsdb_type_help, {
+            helpModal(
+                "Identifier type",
+                stringr::str_c(
+                    "Type of the target signatures in BugSigDB.",
+                    " The type must match the input IDs. ",
+                    "<a href='?tab=help&anchor=#options' target='_blank'>More...</a>"
+                )
+            )
+        }),
+        shiny::observeEvent(input$bsdb_rank_help, {
+            helpModal(
+                "Select taxonomic rank(s)",
+                stringr::str_c(
+                    "Select the rank(s) of the taxa included in the target BugSigDB signature.",
+                    " Use the '(De)select all' check box to select or deselect all ranks at once. ",
+                    "<a href='?tab=help&anchor=#options' target='_blank'>More...</a>"
+                )
+            )
+        }),
+        shiny::observeEvent(input$bsdb_exact_help, {
+            helpModal(
+                "Use exact taxonomiic level",
+                stringr::str_c(
+                    "If 'Yes', only ranks manually curated will be included.",
+                    " If 'No', the taxonomic tree will be cut at the specified rank (above).",
+                    " Only one rank (above) can be selected when the 'No' options is used. ",
+                    "<a href='?tab=help&anchor=#options' target='_blank'>More...</a>"
+                )
+            )
+        }),
+        shiny::observeEvent(input$bsdb_min_help, {
+            helpModal(
+                "Minimum signature size",
+                stringr::str_c(
+                    "Minimum number of IDs to filter the target BugSigDB signatures. ",
+                    "<a href='?tab=help&anchor=#options' target='_blank'>More...</a>"
+                )
+            )
+        })
     )
 }
